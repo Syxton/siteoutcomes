795681651ced2eebc4421e0cb28375d3fc72e5d0
Matt Davidson <davidso1@rose-hulman.edu>
2017-06-28 11:23:46 -0400

    travis ci fixes

--- index.php
+++ index.php
@@ -23,9 +23,9 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-include_once('../../config.php');
+require('../../config.php');
 require_once($CFG->libdir . '/gradelib.php');
-require_once $CFG->dirroot.'/grade/lib.php';
+require_once($CFG->dirroot.'/grade/lib.php');
 
 $outcomeid  = optional_param('outcomeid', 0, PARAM_INT);
 $datefrom   = optional_param_array('datefrom', array(), PARAM_INT);
@@ -43,18 +43,18 @@ require_capability('report/siteoutcomes:view', $context);
 $mform = new \report_siteoutcomes\filter_form(null);
 
 if ($mform->is_submitted() && !empty($outcomeid)) {
-    // Select standard outcome
+    // Select standard outcome.
     $outcome = $DB->get_record('grade_outcomes', array('id' => $outcomeid));
     $scale = new grade_scale(array('id' => $outcome->scaleid), false);
 
     // Grab all courses that use selected standard outcome.
     $courses = $DB->get_records('grade_outcomes_courses', array('outcomeid' => $outcomeid));
 
-    $report_info = array();
+    $reportinfo = array();
     $csv = array();
-    $report_info['outcome'] = $outcome;
-    $report_info["items"] = array();
-    $report_info["categoryavgs"] = array();
+    $reportinfo['outcome'] = $outcome;
+    $reportinfo["items"] = array();
+    $reportinfo["categoryavgs"] = array();
 
     // Grab all outcome items and merge them.
     foreach ($courses as $course) {
@@ -83,7 +83,9 @@ if ($mform->is_submitted() && !empty($outcomeid)) {
         }
 
         // Get grade_items that use each outcome.
-        $checkthis = $DB->get_records_select('grade_items', "outcomeid = ? AND courseid = ? $datesql", array($outcomeid, $course->courseid));
+        $checkthis = $DB->get_records_select('grade_items',
+                                             "outcomeid = ? AND courseid = ? $datesql",
+                                             array($outcomeid, $course->courseid));
         if (!empty($checkthis)) {
             $report_items[$course->courseid] = $checkthis;
         }
@@ -121,20 +123,21 @@ if ($mform->is_submitted() && !empty($outcomeid)) {
             }
         }
 
-        if(empty($report_items[$course->courseid])) {
+        if (empty($report_items[$course->courseid])) {
             unset($report_items[$course->courseid]);
         } else {
              // Merge outcomes data.
-            $report_info["items"] += $report_items;
+            $reportinfo["items"] += $report_items;
         }
     }
 
-    if(empty($report_info["items"])) {
+    if (empty($reportinfo["items"])) {
         print_grade_page_head($COURSE->id, 'report', 'siteoutcomes', get_string('pluginname', 'report_siteoutcomes'), false, '');
         echo "No Data";
     } else {
         $html = '<h3>Course Outcomes Report</h3>';
-        $html .= '<table class="generaltable boxaligncenter" width="90%" cellspacing="1" cellpadding="5" summary="=Course Outcomes Report">' . "\n";
+        $html .= '<table class="generaltable boxaligncenter" ' .
+                 'width="90%" cellspacing="1" cellpadding="5" summary="Course Outcomes Report">' . "\n";
         $html .= '<th class="header c1" scope="col">' . get_string('courseavg', 'grades') . '</th>';
         $html .= '<th class="header c2" scope="col">' . get_string('activities', 'grades') . '</th>';
         $html .= '<th class="header c3" scope="col">' . get_string('average', 'grades') . '</th>';
@@ -147,8 +150,8 @@ if ($mform->is_submitted() && !empty($outcomeid)) {
                            get_string('average', 'grades') . " Numeric",
                            get_string('numberofgrades', 'grades'),
                            'header' => true);
-        
-        foreach ($report_info["items"] as $courseid => $outcomedata) {
+
+        foreach ($reportinfo["items"] as $courseid => $outcomedata) {
             $course = $DB->get_record('course', array("id" => $courseid));
             $coursegradecount = 0;
             $rowspan = count($outcomedata, COUNT_RECURSIVE);
@@ -156,47 +159,47 @@ if ($mform->is_submitted() && !empty($outcomeid)) {
             if ($rowspan == 0) {
                 $rowspan = 1;
             }
-    
-            $report_info['outcome']->sum = 0;
-            
-    
-            $print_tr = false;
-            $items_html = '';
+
+            $reportinfo['outcome']->sum = 0;
+
+            $printtr = false;
+            $itemshtml = '';
 
             if (!empty($outcomedata)) {
                 foreach ($outcomedata as $itemid => $item) {
-                    if ($print_tr) {
+                    if ($printtr) {
                         $row++;
-                        $items_html .= "<tr class=\"r$row\">\n";
+                        $itemshtml .= "<tr class=\"r$row\">\n";
                     }
-
-                    $grade_item = new grade_item($item, false);
         
                     if ($item->itemtype == 'mod') {
                         $cm = get_coursemodule_from_instance($item->itemmodule, $item->iteminstance, $item->courseid);
                         $itemname = '<a href="'.$CFG->wwwroot.'/mod/'.$item->itemmodule.'/view.php?id='.$cm->id.'">'.format_string($cm->name, true, $cm->course).'</a>';
                     } else {
-                        $itemname = $grade_item->get_name();
+                        $gradeitem = new grade_item($item, false);
+                        $itemname = $gradeitem->get_name();
                     }
 
-                    $report_info['outcome']->sum += $item->avg;
+                    $reportinfo['outcome']->sum += $item->avg;
                     $gradehtml = $scale->get_nearest_item($item->avg);
-        
-                    $items_html .= "<td class=\"cell c2\">$itemname</td>"
+
+                    $itemshtml .= "<td class=\"cell c2\">$itemname</td>"
                                  . "<td class=\"cell c3\">$gradehtml ($item->avg)</td>"
                                  . "<td class=\"cell c4\">$item->count</td></tr>\n";
-                    $csv[$row+1] = array($course->fullname,
-                                         mb_convert_encoding(str_replace('"', '""', format_string($cm->name, true, $cm->course)), 'UTF-16LE', 'UTF-8'),
+                    $csv[$row + 1] = array($course->fullname,
+                                         mb_convert_encoding(str_replace('"', '""', format_string($cm->name, true, $cm->course)),
+                                                             'UTF-16LE',
+                                                             'UTF-8'),
                                          $gradehtml,
                                          $item->avg,
                                          $item->count,
                                          'header' => false);
                     $coursegradecount += $item->count;
-                    $print_tr = true;
+                    $printtr = true;
                 }
             } else {
-                $items_html .= "<td class=\"cell c2\"> - </td><td class=\"cell c3\"> - </td><td class=\"cell c4\"> 0 </td></tr>\n";
-                $csv[$row+1] = array($course->fullname,
+                $itemshtml .= "<td class=\"cell c2\"> - </td><td class=\"cell c3\"> - </td><td class=\"cell c4\"> 0 </td></tr>\n";
+                $csv[$row + 1] = array($course->fullname,
                      "-",
                      "-",
                      "-",
@@ -207,26 +210,28 @@ if ($mform->is_submitted() && !empty($outcomeid)) {
             if (is_array($outcomedata)) {
                 $count = count($outcomedata);
                 if ($count > 0) {
-                    $avg = $report_info['outcome']->sum / $count;
+                    $avg = $reportinfo['outcome']->sum / $count;
                 } else {
-                    $avg = $report_info['outcome']->sum;
+                    $avg = $reportinfo['outcome']->sum;
                 }
-                $avg_html = $scale->get_nearest_item($avg) . " (" . round($avg, 2) . ")\n";
+                $avghtml = $scale->get_nearest_item($avg) . " (" . round($avg, 2) . ")\n";
             } else {
-                $avg_html = ' - ';
+                $avghtml = ' - ';
             }
 
-            $report_info["categoryavgs"][$course->category][] = array('avg' => $avg, 'count' => $coursegradecount);
-            $outcomeavg_html = '<td class="cell c1" rowspan="' . $rowspan . '"><strong>' . $course->fullname . "</strong><br /><br />" . $avg_html . "</td>\n";
+            $reportinfo["categoryavgs"][$course->category][] = array('avg' => $avg, 'count' => $coursegradecount);
+            $outcomeavg_html = '<td class="cell c1" rowspan="' . $rowspan . '">' .
+                               '<strong>' . $course->fullname . "</strong><br /><br />" . $avghtml . "</td>\n";
 
-            $html .= $outcomeavg_html . $items_html;
+            $html .= $outcomeavg_html . $itemshtml;
             $row++;   
         }
-    
+
         $html .= '</table><br /><br />';
 
         $html .= '<h3>Category Outcomes Report</h3>';
-        $html .= '<table class="generaltable boxaligncenter" width="90%" cellspacing="1" cellpadding="5" summary="Category Outcomes Report">' . "\n";
+        $html .= '<table class="generaltable boxaligncenter" width="90%" cellspacing="1" cellpadding="5" ' .
+                 'summary="Category Outcomes Report">' . "\n";
         $html .= '<th class="header c1" scope="col">Category</th>';
         $html .= '<th class="header c2" scope="col">Courses</th>';
         $html .= '<th class="header c3" scope="col">' . get_string('average', 'grades') . '</th>';
@@ -242,10 +247,10 @@ if ($mform->is_submitted() && !empty($outcomeid)) {
         $totalcourses = 0;
         $totalaverage = 0;
         $totalgrades = 0;
-        foreach ($report_info["categoryavgs"] as $categoryid => $categorydata) {
+        foreach ($reportinfo["categoryavgs"] as $categoryid => $categorydata) {
             $categoryavgsum = 0; $categorycountsum = 0;
             $coursecount = count($categorydata);
-            foreach($categorydata as $coursedata) {
+            foreach ($categorydata as $coursedata) {
                 $categoryavgsum += $coursedata["avg"];
                 $categorycountsum += $coursedata["count"];
             }
@@ -256,7 +261,7 @@ if ($mform->is_submitted() && !empty($outcomeid)) {
                   . "<td class=\"cell c2\">$coursecount</td>"
                   . "<td class=\"cell c3\">$gradehtml ($categoryavg)</td>"
                   . "<td class=\"cell c4\">$categorycountsum</td></tr>\n";
-            $csv[$row+1] = array($cat->name,
+            $csv[$row + 1] = array($cat->name,
                              $coursecount,
                              $gradehtml,
                              $categoryavg,
@@ -286,14 +291,14 @@ if ($mform->is_submitted() && !empty($outcomeid)) {
         $html .= "<td class=\"cell c1\">$totalcourses</td>"
                   . "<td class=\"cell c2\">$gradehtml ($totalaverage)</td>"
                   . "<td class=\"cell c3\">$totalgrades</td></tr>\n";
-        $csv[$row+1] = array($totalcourses,
+        $csv[$row + 1] = array($totalcourses,
                              $gradehtml,
                              $totalaverage,
                              $totalgrades,
                              'header' => false);
         $html .= '</table>';
         echo $OUTPUT->header();
-        echo $OUTPUT->heading("\"" . $report_info['outcome']->shortname . "\" Outcome Report");
+        echo $OUTPUT->heading("\"" . $reportinfo['outcome']->shortname . "\" Outcome Report");
         $SESSION->siteoutcomes_export_values = $csv;
         echo html_writer::link(new moodle_url('export.php',
                                               array()), get_string('downloadexcel'));
@@ -302,12 +307,12 @@ if ($mform->is_submitted() && !empty($outcomeid)) {
 
     $event = \report_siteoutcomes\event\report_viewed::create(
         array(
-            'other' =>  array('requestedoutcome' => $outcomeid)
+            'other' => array('requestedoutcome' => $outcomeid)
         )
     );
     $event->trigger();
 
-} else { // FORM
+} else { // FORM.
     $outcomeids = $DB->get_records_select_menu('grade_outcomes', "courseid IS NULL", null, 'shortname ASC', 'id, shortname');
     $outcomeids = array(0 => get_string('selectoutcome', 'report_siteoutcomes')) + $outcomeids;
 
@@ -316,7 +321,7 @@ if ($mform->is_submitted() && !empty($outcomeid)) {
     $filters = array();
     if ($data = $mform->get_data()) {
         $filters = (array)$data;
-    
+
         if (!empty($filters['datetill'])) {
             $filters['datetill'] += DAYSECS - 1; // Set to end of the chosen day.
         }
@@ -336,7 +341,6 @@ if ($mform->is_submitted() && !empty($outcomeid)) {
     echo $OUTPUT->header();
     echo $OUTPUT->heading(get_string('pluginname', 'report_siteoutcomes'));
     $mform->display();
-
 }
 
 echo $OUTPUT->footer();
