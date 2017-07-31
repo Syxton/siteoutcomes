428d4d352d31211356b07ff31a56f51a912f88c9
Matt Davidson <davidso1@rose-hulman.edu>
2017-07-31 09:33:36 -0400

    Version bump

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2017051500;            // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2017073100;            // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2015111600;            // Requires Moodle 3.x.
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->release   = '1';
